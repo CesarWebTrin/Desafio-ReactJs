f18aef09da359d63103c6a33ad304b8340a73658
root <root@CESAR-PC.localdomain>
2020-06-20 16:06:53 -0300

    PROJETO FINALIZADO

--- src/App.js
+++ src/App.js
@@ -1,26 +1,67 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import api from './services/api';
 
 import "./styles.css";
 
+
 function App() {
-  async function handleAddRepository() {
+
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(()=>{
+    api.get('/repositories').then(response =>{
+      setRepositories(response.data);
+      //console.log(response);
+    });
+  }, []);
+
+
+
+   async function handleAddRepository() {
     // TODO
+
+     const response = await api.post('/repositories',{
+
+      title:"Novo projeto2", 
+      url:"https://github.com/CesarWebTrin/conceitos-nodejs.git", 
+      techs: "Node js"
+    });
+
+    const repository = response.data;
+
+    setRepositories([...repositories, repository]); 
+
+    /* api.post('repositories', {
+      title:"Novo projeto", 
+      url:"https://github.com/CesarWebTrin/conceitos-nodejs.git", 
+      techs: "Node js"
+    }); */
+
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    
+    api.delete(`/repositories/${id}`).then(reponse =>{
+      
+        const newRepositories = repositories.filter(repository => repository.id !== id);
+
+        setRepositories(newRepositories);
+      
+    })
+
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
 
-          <button onClick={() => handleRemoveRepository(1)}>
+        {repositories.map(repository => <li key={repository.id}> 
+            <li>{repository.title}</li>)
+          <button onClick={() => handleRemoveRepository(repository.id)}>            
             Remover
           </button>
         </li>
+        )}            
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
